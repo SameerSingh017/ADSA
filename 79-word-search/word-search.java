2516dfa668c4275cf303114808b7366a251e54b1
Sameer Singh <sameer0555singh@gmail.com>
2026-01-21 01:57:29 +0530

    Time: 136 ms (61.44%) | Memory: 43 MB (67.65%) - LeetSync

---
+++ 79-word-search/word-search.java
@@ -0,0 +1,29 @@
+class Solution {
+    public boolean exist(char[][] board, String word) {
+        int n = board.length;
+        int m = board[0].length;
+        for(int i=0; i<n; i++){
+            for(int j=0; j<m ; j++){
+                if(board[i][j] == word.charAt(0)){
+                    if(dfs(i,j,0,board,word)){
+                        return true;
+                    }
+                }
+            }
+        }
+        return false;
+    }
+    public boolean dfs(int i, int j, int idx, char[][] board, String word){
+        if(idx == word.length()){
+            return true;
+        }
+        if(i<0 || j<0 || i>=board.length || j>=board[0].length || word.charAt(idx) != board[i][j]){
+            return false;
+        }
+        char temp = board[i][j];
+        board[i][j] = '#';
+        boolean found = dfs(i,j+1,idx+1,board,word) || dfs(i,j-1,idx+1,board,word) ||dfs(i+1,j,idx+1,board,word) || dfs(i-1,j,idx+1,board,word);
+        board[i][j] = temp;
+        return found;
+    }
+}
