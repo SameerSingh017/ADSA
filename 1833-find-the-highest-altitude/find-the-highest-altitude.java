3fa9d9352f6bbb4bb399710de275be13274bbbad
Sameer Singh <sameer0555singh@gmail.com>
2026-02-19 16:48:04 +0530

    Time: 0 ms (100.00%) | Memory: 43.2 MB (37.44%) - LeetSync

--- 1833-find-the-highest-altitude/find-the-highest-altitude.java
+++ 1833-find-the-highest-altitude/find-the-highest-altitude.java
@@ -10,3 +10,15 @@ class Solution {
 
     }
 }
+
+// class Solution {
+//     public int largestAltitude(int[] gain) {
+//         int max = 0;
+//         for(int i=1 ; i<gain.length; i++){
+//             gain[i] = gain[i-1] + gain[i];
+//             max = Math.max(max, gain[i]);
+//         }
+//         return max;
+
+//     }
+// }
