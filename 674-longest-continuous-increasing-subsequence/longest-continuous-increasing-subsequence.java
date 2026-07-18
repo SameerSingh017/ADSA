19d6d2fa582a84051f5c7ae818f1bc8f64a9e2d9
Sameer Singh <sameer0555singh@gmail.com>
2026-07-18 20:16:42 +0530

    Time: 2 ms (46.66%) | Memory: 46.9 MB (52.76%) - LeetSync

---
+++ 674-longest-continuous-increasing-subsequence/longest-continuous-increasing-subsequence.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int findLengthOfLCIS(int[] nums) {
+        int currLen = 1;
+        int maxLen = 1;
+        for(int i=1; i<nums.length; i++){
+            if(nums[i] > nums[i-1]){
+                currLen++;
+            }else{
+                currLen = 1;
+            }
+            maxLen = Math.max(maxLen, currLen);
+        }
+        return maxLen;
+    }
+}
